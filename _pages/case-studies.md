36f4fb11bae4a81e142c11334b5fd16e3e8437be
Aditya <adigeak29@gmail.com>
2025-10-19 12:30:10 +0530

    v1.0

---
+++ _pages/case-studies.md
@@ -0,0 +1,8 @@
+---
+title: "Case Studies"
+layout: case-studies
+permalink: /case-studies/
+author_profile: true
+---
+
+Here is a collection of case studies I have worked on. You can use the search box to filter by title, category, difficulty, or framework.
---
+++ assets/js/datatables-init.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // Only initialize DataTable if the table exists on the page
+  if ($('#case-studies-table').length) {
+    $('#case-studies-table').DataTable({
+      "paging": false,
+      "order": [[ 4, "desc" ]], // Sort by the 5th column (Date Solved) descending
+      "info": false, // Hide "Showing X of Y entries"
+      "columnDefs": [
+        { "orderable": false, "targets": 3 } // Disable sorting on the 4th column (Tags)
+      ]
+    });
+  }
+});
